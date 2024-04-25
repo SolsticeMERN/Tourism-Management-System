76bc1bbf7ef0787505d8bc28d93c3ee910a76023
Shakillazycoder <sakil.khan882.sk@gmail.com>
2024-04-26 03:17:05 +0600

    footer section complete

--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -1,11 +1,17 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Components/Navbar";
+import Footer from "../Pages/Footer";
 
 const Main = () => {
     return (
         <div>
+            <div className="container mx-auto m-4 px-4">
             <Navbar></Navbar>
+            <div className="min-h-[calc(100vh-375px)]">
             <Outlet></Outlet>
+            </div> 
+        </div>
+        <Footer></Footer>
         </div>
     );
 };
