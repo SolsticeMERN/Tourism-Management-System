88226c76deaae701e6bef0c4c1cf0caee1d86cda
Shakillazycoder <sakil.khan882.sk@gmail.com>
2024-04-30 22:48:19 +0600

    some information add on card

--- src/Components/TouristSport.jsx
+++ src/Components/TouristSport.jsx
@@ -1,4 +1,5 @@
 import { Helmet } from "react-helmet-async";
+import { AiOutlinePound } from "react-icons/ai";
 import { CiCalendar } from "react-icons/ci";
 import { FaLocationDot } from "react-icons/fa6";
 import { IoTimeOutline } from "react-icons/io5";
@@ -14,6 +15,8 @@ const TouristSport = ({ touristic }) => {
     shortdescription,
     travel_time,
     totalVisitorsPerYear,
+    country_Name,
+    seasonality,
     _id,
   } = touristic;
 
@@ -39,31 +42,49 @@ const TouristSport = ({ touristic }) => {
           </a>
         </div>
         <div className="p-6 text-black dark:text-white">
-          <h5 className="mb-2 text-xl text-black font-medium leading-tight">
-            {tourists_spot_name}
-          </h5>
-          <div>
-            <small className="text-black flex items-center mb-3 gap-2">
-              <FaLocationDot />
-              {location}
-            </small>
-          </div>
-          <p className="mb-4 text-black">{shortdescription}</p>
-          <p className="mb-4 text-black">{average_cost}</p>
-          <p className="mb-4 text-black flex item-center gap-2"><IoTimeOutline/> {travel_time}</p>
-          <p className="mb-4 text-black flex item-center gap-2"><MdPeopleOutline></MdPeopleOutline> {totalVisitorsPerYear}</p>
-          <p className="mb-4 text-black flex item-center gap-2"><CiCalendar/> {totalVisitorsPerYear}</p>
-          <Link to={`/viewDetails/${_id}`}>
-            <button
-              type="button"
-              className="inline-block rounded bg-[#1f95ae] px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-[#1f95ae] focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong"
-              data-twe-ripple-init
-              data-twe-ripple-color="light"
-            >
-              View Details
-            </button>
-          </Link>
-        </div>
+                  <h5 className="mb-2 text-xl text-black font-medium leading-tight">
+                    {tourists_spot_name}
+                  </h5>
+                  <div className="flex  gap-5">
+                  <small className="text-black flex mb-2 items-center gap-2">
+                      <FaLocationDot />
+                      {country_Name}
+                    </small>
+                    <small className="text-black flex mb-2 items-center gap-2">
+                      <FaLocationDot />
+                      {location}
+                    </small>
+                  </div>
+                  <p className="mb-4 text-black">{shortdescription}</p>
+                  <div className="text-black flex gap-5 mb-4">
+                    <span className="flex items-center gap-2">
+                    <AiOutlinePound/>  {average_cost}
+                    </span>
+                    <p className="flex items-center gap-2">
+                    <CiCalendar/> {seasonality}
+                    </p>
+                  </div>
+                  <div className="text-black flex gap-5 mb-4">
+                    <span className="flex items-center gap-2">
+                    <MdPeopleOutline/>  {totalVisitorsPerYear}
+                    </span>
+                    <p className="flex items-center gap-2">
+                    <IoTimeOutline/> {travel_time}
+                    </p>
+                  </div>
+                  <div>
+                    <Link to={`/viewDetails/${_id}`}>
+                      <button
+                        type="button"
+                        className="inline-block btn rounded bg-[#1f95ae] px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong"
+                        data-twe-ripple-init
+                        data-twe-ripple-color="light"
+                      >
+                        View Details
+                      </button>
+                    </Link>
+                  </div>
+                </div>
       </div>
     </div>
   );
